406d62ed1fd09e562b70fe3d4b0541efad6a034e
Abogado German Gallego <abogado.germangallego@gmail.com>
2026-03-11 16:46:41 -0500

    feat: add Microsoft Clarity vuahazu2gs to head

--- src/app/layout.js
+++ src/app/layout.js
@@ -45,6 +45,19 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           }}
         />
         {/* End Google Tag Manager */}
+
+        {/* Microsoft Clarity - vuahazu2gs */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "vuahazu2gs");`,
+          }}
+        />
+        {/* End Microsoft Clarity */}
       </head>
       <body>
         {/* Google Tag Manager (noscript) - justo despu\u00e9s de <body> */}
