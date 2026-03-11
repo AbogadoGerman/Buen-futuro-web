a813c4fd92bc78fee13e15d409bf34ecde15a90c
Abogado German Gallego <abogado.germangallego@gmail.com>
2026-03-11 16:38:06 -0500

    feat: add Google Tag Manager GTM-THDMLN24 to head and body

--- src/app/layout.js
+++ src/app/layout.js
@@ -33,13 +33,35 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <head />
+      <head>
+        {/* Google Tag Manager - lo m\u00e1s arriba posible en <head> */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-THDMLN24');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body>
+        {/* Google Tag Manager (noscript) - justo despu\u00e9s de <body> */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-THDMLN24"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         {/*
           TikTok Pixel (D6NG0U3C77U75L0FFE4G) y Meta Pixel (925375136552561)
           se inicializan dentro de PixelScripts.jsx SOLO cuando el usuario
           acepta cookies (cookie_consent === 'accepted').
-          NO se duplica el script aqui para evitar doble disparo de eventos.
         */}
         <PixelScripts />
         {children}
